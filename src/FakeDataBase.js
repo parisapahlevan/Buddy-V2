0a648f9755ff4618f09b412e2e368e1784c39b96
Parisa Pahlevan <paris.pahlevan@gmail.com>
2019-08-03 19:42:03 -0400

    deleted images and added them to my drive

--- src/FakeDataBase.js
+++ src/FakeDataBase.js
@@ -4,41 +4,41 @@ const fakeDogs = [{
     name : 'Tala',
     age : 2,
     breed : 'Golden retriever',
-    image: require('../src/assets/img/0.jpg')
+    image: 'https://drive.google.com/uc?id=1-iOz3YmiKXHlfmXEVXW1-sd84sdqu4LQ'
   },
   {
     name : 'Boo',
     age : 5,
     breed : 'Pitpull',
-    image: require('../src/assets/img/3.jpg')
+    image: 'https://drive.google.com/uc?id=1Oz6RCcvnwR2B_afUr5zqy9HZ8E5dBXmo'
     //image : 'https://t2.ea.ltmcdn.com/en/images/9/0/0/img_names_for_pit_bull_dogs_9_600.jpg'
   },
   {
     name : 'Zouzou',
     age : 8,
     breed : 'Beagle',
-    image: require('../src/assets/img/8.jpg')
+    image: 'https://drive.google.com/uc?id=1z-i103rrV-j3aLOj3GhsEY-iTYgeyHAJ'
     //image : 'http://static.ddmcdn.com/en-us/apl/breedselector/images/breed-selector/dogs/breeds/beagle_04_lg.jpg'
   },
   {
     name : 'Muchi',
     age : 4,
     breed : 'Corgi',
-    image: require('../src/assets/img/1.jpg')
+    image: 'https://drive.google.com/uc?id=1i4fCSJRxgsQWe5jmgJmbhn846QR6d_97'
     //image : 'https://images2.minutemediacdn.com/image/upload/c_crop,h_1194,w_2119,x_0,y_70/f_auto,q_auto,w_1100/v1554738239/shape/mentalfloss/63484-istock-533859316.jpg'
   },
   {
     name : 'sky',
     age : 3,
     breed : 'Husky',
-    image: require('../src/assets/img/99.png')
+    image: 'https://drive.google.com/uc?id=1gDDntzIZr3-E8PoKG-0haY7YstokjZ0K'
     //image : 'https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_960_720.jpg'
   },
   {
     name : 'Prince',
     age : 4,
     breed : 'Labradoodle',
-    image: require('../src/assets/img/65.jpg')
+    image: 'https://drive.google.com/uc?id=1jhle-dr_n-inqAK656mLeOLBS6bw5Hoh'
     //image : 'https://thenypost.files.wordpress.com/2018/09/85327728.jpg?quality=90&strip=all&w=618&h=410&crop=1'
   }
 ];
---
+++ src/assets/img/imageUrls.js
@@ -0,0 +1,22 @@
+var urls = {
+'0'  : 'https://drive.google.com/uc?id=1-iOz3YmiKXHlfmXEVXW1-sd84sdqu4LQ', 
+'1'  : 'https://drive.google.com/uc?id=1i4fCSJRxgsQWe5jmgJmbhn846QR6d_97',
+'01' : 'https://drive.google.com/uc?id=1mENugHAHgrnN3O7Z0Dirz8gH2DcEjWdT',
+'02' : 'https://drive.google.com/uc?id=1SVZEXgYYqHbnoqyH2H01QJ_39__1ILFt',
+'03' : 'https://drive.google.com/uc?id=16_ySoifOpRNa4Mo_b2h075__HfcppKey',
+'3'  : 'https://drive.google.com/uc?id=1Oz6RCcvnwR2B_afUr5zqy9HZ8E5dBXmo',
+'04' : 'https://drive.google.com/uc?id=1voO3Fx66eFDIBSpYkga6XMChKi6rU4tg',
+'5'  : 'https://drive.google.com/uc?id=1SoJ4ttTp7M_nHHYOdtnSsgnINRYh0iQC',
+'7'  : 'https://drive.google.com/uc?id=1FTzrIfVEH548tENE772mCTH651V-_Zwo',
+'8'  : 'https://drive.google.com/uc?id=1z-i103rrV-j3aLOj3GhsEY-iTYgeyHAJ',
+'9'  : 'https://drive.google.com/uc?id=1MX4TSx0BDaxlKa-b58Xn9YTMWcaLqg20',
+'22' : 'https://drive.google.com/uc?id=1oD89tQJPXOFiRcLPL6INtuSl8cgi-CwE',
+'23' : 'https://drive.google.com/uc?id=19tDvbWUyiFuCZantCGuE0FdlPVLtJWAj',
+'24' : 'https://drive.google.com/uc?id=1ftKHz4O5MNkoxST3KLnj4E3uzczSR7yr',
+'25' : 'https://drive.google.com/uc?id=1d-kGhAaQ-sCaFPXHr49UTKoQskPK5lFV',
+'44' : 'https://drive.google.com/uc?id=1WP1AmHRwPxPQaADrvvtS2LyXFumTaN9p',
+'65' : 'https://drive.google.com/uc?id=1jhle-dr_n-inqAK656mLeOLBS6bw5Hoh',
+'77' : 'https://drive.google.com/uc?id=1_ZZ8G8cM99XxBF3bhuujX3iJi1c03GnD',
+'99' : 'https://drive.google.com/uc?id=1gDDntzIZr3-E8PoKG-0haY7YstokjZ0K'
+
+}
