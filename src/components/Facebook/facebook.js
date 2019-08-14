3e85873b5bd0606f0eb245e6a9ad980385496058
Parisa Pahlevan <paris.pahlevan@gmail.com>
2019-08-14 14:21:39 -0400

    added facebook and google signup

--- src/components/Facebook/facebook.js
+++ src/components/Facebook/facebook.js
@@ -1,6 +1,7 @@
 
-import FacebookLogin from 'react-facebook-login';
+// import FacebookLogin from 'react-facebook-login';
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 
 export default class Facebook extends Component {
 
@@ -45,8 +46,13 @@ export default class Facebook extends Component {
                 appId="393434351376565"
                 autoLoad={true}
                 fields="name,email,picture"
-                onClick={this.myOnClick.bind(this)}
-                callback={this.myCallBack.bind(this)} />)
+                // onClick={this.myOnClick.bind(this)}
+                callback={this.myCallBack.bind(this)} 
+                render={renderProps => (
+                    <button id="facebookButton"  onClick={renderProps.onClick}><svg aria-hidden="true" id="logodesign" class="svg-icon iconFacebook" width="18" height="18" viewBox="0 0 18 18"><path d="M1.88 1C1.4 1 1 1.4 1 1.88v14.24c0 .48.4.88.88.88h7.67v-6.2H7.46V8.4h2.09V6.61c0-2.07 1.26-3.2 3.1-3.2.88 0 1.64.07 1.87.1v2.16h-1.29c-1 0-1.19.48-1.19 1.18V8.4h2.39l-.31 2.42h-2.08V17h4.08c.48 0 .88-.4.88-.88V1.88c0-.48-.4-.88-.88-.88H1.88z" fill="#3C5A96"></path></svg>Sign in using Facebook</button>
+                  )}
+                />
+                )
         }
         return (
             <div>
